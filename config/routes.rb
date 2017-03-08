05c184b178f1aebde615e75694484ff52c046cb7
Fabien_D <supernovae13@gmail.com>
2017-03-08 12:47:27 +0100

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  root 'static_pages#home' # This arranges for requests for / to be routed to the home action in the Static Pages controller.
   get 'static_pages/home'
   get 'static_pages/help'
-  root 'application#hello'
+  get  'static_pages/about'
+  get  'static_pages/contact'
 end
