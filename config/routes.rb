3e70435502354aca615109a2621220e7341ed703
Fabien_D <supernovae13@gmail.com>
2017-03-13 15:57:52 +0100

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,25 @@
+#Tuto rooting :
+#http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
+  get 'users/new'
+
   root 'static_pages#home' # This arranges for requests for / to be routed to the home action in the Static Pages controller.
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get  '/help', to: 'static_pages#help'   
+  get  '/about', to: 'static_pages#about'   
+  get  '/contact', to: 'static_pages#contact'   
+  get  '/signup',  to: 'users#new'
 end
+
+
+
+#get "/posts" => "posts#index"
+#get "/posts/:id" => "posts#show"
+#get "/posts/new" => "posts#new"
+#post "/posts" => "posts#create"  # usually a submitted form
+#get "/posts/:id/edit" => "posts#edit"
+#put "/posts/:id" => "posts#update" # usually a submitted form
+#delete "/posts/:id" => "posts#destroy"
+
+#Equivalent à :
+#resources :posts
